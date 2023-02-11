33561607ca7210e5547c29f1c5fcb9de348b0a8b
millsguo <guomin@yundianyi.com>
2023-02-11 21:19:51 +0800

    修复字幕翻译bug

--- library/EasySub/src/CheckSub.php
+++ library/EasySub/src/CheckSub.php
@@ -119,8 +119,8 @@ class CheckSub
                             $enableTrans = $_ENV['ENABLE_TRANS'] ?? 'false';
 
                             if (strtolower($enableTrans) === 'true' || $enableTrans === true) {
-                                Log::info('开始翻译英文字幕文件:' . $fullPath . '/' . $engSubFile);
-                                TransSub::transSubFile($fullPath . '/' . $engSubFile, $chineseSubFileName, 'eng', 'zh');
+                                Log::info('开始翻译英文字幕文件:' . $engSubFile);
+                                TransSub::transSubFile($engSubFile, $chineseSubFileName, 'eng', 'zh');
                             } else {
                                 Log::info('翻译功能已关闭');
                             }
